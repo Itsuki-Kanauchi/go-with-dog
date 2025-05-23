ea52c8e49e6b7bcf5eb2ebcf10f8798c0b52ce4e
中西遥香 <hrs8104@nakanishiharukanoMacBook-Pro.local>
2025-05-23 16:22:35 +0900

    Postsモデル作成・マイグレーション実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, length: { maximum: 255 }
 
+  has_many :posts, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
