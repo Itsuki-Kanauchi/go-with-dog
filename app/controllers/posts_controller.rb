11f0b599d87e643a328b28168dcd721b7cf09460
中西遥香 <hrs8104@nakanishiharukanoMacBook-Pro.local>
2025-05-24 22:11:57 +0900

    createアクション作成・一覧のエラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,25 @@ class PostsController < ApplicationController
   end
 
   def new 
-    @posts = Post.new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: "投稿が完了しました！"
+    else
+      flash.now[:danger] = "投稿に失敗しました。入力内容を確認してください。"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :address, :body, :image)
   end
 end
