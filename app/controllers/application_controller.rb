f972bd46d4c96063b7b5f821f445e3930d011a6c
中西遥香 <hrs8104@nakanishiharukanoMacBook-Pro.local>
2025-05-29 17:58:00 +0900

    フラッシュの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  add_flash_types :success, :danger
+
   protected
 
   def configure_permitted_parameters
