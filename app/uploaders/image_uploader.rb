ce607b8a5480f3c51384a1dced8c5172b19609a8
中西遥香 <hrs8104@nakanishiharukanoMacBook-Pro.local>
2025-05-30 22:33:40 +0900

    carrierwave導入・uploaderファイル/モデルファイル編集

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,16 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  # アップロードされたファイルの保存ディレクトリ
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # ファイルがアップロードされていないときのデフォルト画像
+  def default_url
+    'sample.jpg' # publicディレクトリ配下のパスが対象（例: public/sample.jpg）
+  end
+
+  # 許可する拡張子のホワイトリスト
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+end
